a36341a484bb4b619fa279c6b6699edb8c6b60a4
Leandro Cruz <leandro.programmer@hotmail.com>
2021-01-11 20:24:09 -0300

    update image on MobileHeader

--- src/components/MobileHeader/index.tsx
+++ src/components/MobileHeader/index.tsx
@@ -5,7 +5,7 @@ import { Container, ProfileCircle, SearchInput, MessageIcon } from "./styles";
 const MobileHeader: React.FC = () => {
   return (
     <Container>
-      <ProfileCircle src="https://github.com/guilhermerodz.png" />
+      <ProfileCircle src="https://avatars1.githubusercontent.com/u/59587859?s=400&u=e2c61934c682f1bc9a5d07dfb9cb172bf3cf8b9c&v=4" />
       <SearchInput placeholder="Pesquisar" />
       <MessageIcon />
     </Container>
